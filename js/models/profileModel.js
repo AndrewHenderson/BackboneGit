1ef313f3b7521d6d62c6ee02792ce1a5428f8aa6
Andrew Henderson <andrew.henderson@chromeriver.com>
2012-11-12 18:14:01 -0800

    Implemented warning state and timestamping

--- js/models/profileModel.js
+++ js/models/profileModel.js
@@ -10,8 +10,8 @@ define(['backbone'], function ( Backbone ) {
 			last: 'Henderson',
 			city: 'Los Angeles',
 			state: 'California',
-			bio: 'Sartorial vegan fixie enim wayfarers. Cardigan officia bicycle rights, beard thundercats small batch mustache salvia cosby sweater enim. American apparel tattooed culpa, duis craft beer vero food truck fingerstache shoreditch ethical gastropub squid seitan. Hoodie high life +1 nulla, cupidatat kogi proident wolf sunt wayfarers irure. Sartorial eu dolor, deserunt before they sold out organic forage master cleanse. Scenester nesciunt iphone delectus blog skateboard. Vice kale chips minim pinterest bespoke.'
-
+			bio: 'Sartorial vegan fixie enim wayfarers. Cardigan officia bicycle rights, beard thundercats small batch mustache salvia cosby sweater enim. American apparel tattooed culpa, duis craft beer vero food truck fingerstache shoreditch ethical gastropub squid seitan. Hoodie high life +1 nulla, cupidatat kogi proident wolf sunt wayfarers irure. Sartorial eu dolor, deserunt before they sold out organic forage master cleanse. Scenester nesciunt iphone delectus blog skateboard. Vice kale chips minim pinterest bespoke.',
+			timestamp: 'Mon Nov 12 2012 11:30:00 GMT-0800 (Pacific Standard Time)'
 		}
 	
 	});
